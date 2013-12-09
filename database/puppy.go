e2e960f5703b7a46ba9c22a56518dd57e8a13b1c
Ruchi Varshney <rvarshney@hearsaycorp.com>
2013-12-08 20:46:02 -0800

    Puppy API web app

---
+++ database/puppy.go
@@ -0,0 +1,62 @@
+package database
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+	"strings"
+)
+
+type Puppy struct {
+	ImageUrl string
+	ImageType string
+	Breed string
+}
+
+var db *sql.DB
+
+func InitPuppyDB() {
+	if db == nil {
+		// Get DB settings from OS env
+		dbHost := os.Getenv("DB_HOST")
+		dbUser := os.Getenv("DB_USER")
+		dbPwd := os.Getenv("DB_PWD")
+		dbName := os.Getenv("DB_NAME")
+		dbPort := os.Getenv("DB_PORT")
+		dbStr := "host='" + dbHost + "' user='" + dbUser + "' password='" + dbPwd + "' dbname='" + dbName + "' port='" + dbPort + "'"
+
+		var err error
+		db, err = sql.Open("postgres", dbStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	log.Println(db)
+}
+
+func GetRandomPuppy(breed string, imageType string) *Puppy {
+	// Perform LIKE query to pick a random puppy. Yes, this is pretty darn inefficient.
+	rows, err := db.Query(`SELECT image_url, image_type, breed FROM public.thepuppyapi_puppies WHERE breed LIKE $1 AND image_type LIKE $2 ORDER BY random() LIMIT 1`, "%"+strings.ToLower(breed)+"%", "%"+strings.ToLower(imageType)+"%")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Extract results
+	var resultImageUrl string
+	var resultImageType string
+	var resultBreed string
+	for rows.Next() {
+		err = rows.Scan(&resultImageUrl, &resultImageType, &resultBreed)
+	}
+	return &Puppy{ImageUrl: resultImageUrl, ImageType: resultImageType, Breed: resultBreed}
+}
+
+func AddPuppy(puppy *Puppy) {
+	// Attempts to perform an insert into the puppy database
+	log.Printf("database.AddPuppy: Trying to add image_url=%v image_type=%v breed=%v", puppy.ImageUrl, puppy.ImageType, puppy.Breed)
+	_, err := db.Query(`INSERT INTO public.thepuppyapi_puppies (image_url, image_type, breed) VALUES ($1, $2, $3)`, puppy.ImageUrl, puppy.ImageType, strings.ToLower(puppy.Breed))
+	if err != nil {
+		log.Printf("database.AddPuppy: %v", err)
+	}
+}
---
+++ web/web.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"thepuppyapi/database"
+	"encoding/json"
+	"html/template"
+	"log"
+	"net/http"
+	"os"
+)
+
+func Index(res http.ResponseWriter, req *http.Request) {
+	t, _ := template.ParseFiles("web/static/templates/index.html")
+	randomPuppy := database.GetRandomPuppy("", "")
+	t.Execute(res, randomPuppy.ImageUrl)
+}
+
+func Puppy(res http.ResponseWriter, req *http.Request) {
+	// Parse GET request
+	err := req.ParseForm()
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Grab GET form data
+	params := req.Form
+
+	// Parse out the breed, if present
+	var breed []string
+	breed = params["breed"]
+	if breed == nil {
+		breed = []string{""}
+	}
+
+	// Parse out the image type, if present
+	var imageType []string
+	imageType = params["type"]
+	if imageType == nil {
+		imageType = []string{""}
+	}
+
+	var format []string
+	format = params["format"]
+	if format == nil {
+		format = []string{"json"}
+	}
+
+	log.Printf("web.Puppy: breed=%v type=%v format=%v", breed[0], imageType[0], format[0])
+
+	// Get a random puppy with the constraints
+	randomPuppy := database.GetRandomPuppy(breed[0], imageType[0])
+
+	// If image source, redirect to the puppy image
+	if format[0] == "src" {
+		http.Redirect(res, req, randomPuppy.ImageUrl, http.StatusFound)
+	}
+
+	// Return JSON
+	response := map[string]string{"puppy_url": randomPuppy.ImageUrl}
+	data, err := json.Marshal(response)
+	if err != nil {
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	res.Header().Set("Content-Type", "application/json; charset=utf-8")
+	res.Write(data)
+}
+
+func main() {
+	// Set up database connection
+	log.Println("Setting up DB...")
+	database.InitPuppyDB()
+
+	// Set up URL routes
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/puppy", Puppy)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("web/static"))))
+
+	// Start the server
+	log.Println("Listening...")
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
